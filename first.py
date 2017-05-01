a03590510d6e0750d9f080f064d67bcbb9275a7d
VimPy <rawlinsfeng@163.com>
2017-05-01 02:24:10 -0700

    add first.py,the purpose is testing different name of directory

---
+++ first.py
@@ -0,0 +1,10 @@
+a=1
+b=2
+print(a+b)
+print("hello python-mode")
+import os
+path='./python'
+os.mkdir(path)
+os.chdir(path)
+print(map(lambda x : x+'(wanguangyuan)',['hejiuhaojie','tengchuanbo']))
+print(type(map))
